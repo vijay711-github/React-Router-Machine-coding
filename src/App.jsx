dfc5bb0d784202c70f22fbef2b9de672ca1045be
vijay711-github <vj7112000@gmail.com>
2023-09-07 23:54:29 +0530

    Initalized React Router

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Vijay</h1>
+    </>
+  )
+}
+
+export default App
